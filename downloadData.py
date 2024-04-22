87cd134e4d301277325618fc252ccd8ef7517667
julielbr <83019635+julielbr@users.noreply.github.com>
2024-04-22 12:57:40 +0200

    fixed annotation of downloadData.py
    fixed annotation of download data from ClinVar. Used snpEff for annotating

--- downloadData.py
+++ downloadData.py
@@ -1,46 +1,79 @@
+import logging
+import os
 import requests
-import gzip
 import shutil
+import gzip
+import subprocess
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class GenomicDataDownloader:
-    def __init__(self, url, filename, size_limit):
+    def __init__(self, url, filename, size_limit=None):
         self.url = url
         self.filename = filename
-        self.size_limit = size_limit  # Size limit in bytes
+        self.size_limit = size_limit
+
+    def head_request(self):
+        response = requests.head(self.url)
+        response.raise_for_status()
+        file_size = int(response.headers.get('Content-Length', 0))
+        return file_size
 
     def download_file(self):
-        try:
-            with requests.head(self.url) as r:  # Send a HEAD request to get file size
-                file_size = int(r.headers.get('Content-Length', 0))
-            
-            if file_size > self.size_limit:
-                print(f"File exceeds the size limit of {self.size_limit} bytes.")
-                return
+        file_size = self.head_request()
+        if self.size_limit and file_size > self.size_limit:
+            logging.error(f"File exceeds the size limit of {self.size_limit} bytes.")
+            return False
 
-            with requests.get(self.url, stream=True) as r:
-                r.raise_for_status()
-                with open(self.filename, 'wb') as f:
-                    for chunk in r.iter_content(chunk_size=8192):
-                        if chunk:
-                            f.write(chunk)
-            print("Download completed successfully!")
-            self.decompress_file()
-            
-        except requests.exceptions.HTTPError as e:
-            print(f"HTTP error occurred: {e}")
-        except requests.exceptions.RequestException as e:
-            print(f"Error downloading file: {e}")
+        response = requests.get(self.url, stream=True)
+        response.raise_for_status()
+        with open(self.filename, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=8192):
+                if chunk:
+                    file.write(chunk)
+        logging.info(f"Download completed successfully for {self.filename}")
+        return True
 
     def decompress_file(self):
         decompressed_filename = self.filename.rstrip('.gz')
-        with gzip.open(self.filename, 'rb') as f_in:
-            with open(decompressed_filename, 'wb') as f_out:
-                shutil.copyfileobj(f_in, f_out)
-        print(f"File decompressed as {decompressed_filename}")
+        with gzip.open(self.filename, 'rb') as file_in:
+            with open(decompressed_filename, 'wb') as file_out:
+                shutil.copyfileobj(file_in, file_out)
+        logging.info(f"File decompressed as {decompressed_filename}")
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
+
+    def run_annotation(self, vcf_filename):
+        snpEff_command = [
+            'java', '-Xmx8g', '-jar', 'snpEff/snpEff.jar',
+            'GRCh37.75',
+            '-strict',  
+            '-ud', '5000',  
+            vcf_filename
+    ]
+        with open('clinvar_annotated.vcf', 'w') as file_out:
+            process = subprocess.run(snpEff_command, stdout=file_out, stderr=subprocess.PIPE)
+
+        if process.returncode != 0:
+            logging.error(f"SnpEff failed with return code {process.returncode}")
+            logging.error(process.stderr.decode())
+            return False
+
+        logging.info("Annotation with SnpEff completed.")
+        return True
+
 
 if __name__ == "__main__":
     clinvar_url = 'https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/clinvar.vcf.gz'
-    output_path = 'clinvar.vcf.gz'
-    max_file_size = 100 * 1024 * 1024  # 100 MB size limit bc problems with git...
-    downloader = GenomicDataDownloader(clinvar_url, output_path, max_file_size)
-    downloader.download_file()
+    output_filename = 'clinvar.vcf.gz'
+    # Increase the size limit or set it to None to have no limit
+    size_limit = 100 * 1024 * 1024  # 100 MB
+
+    downloader = GenomicDataDownloader(clinvar_url, output_filename, size_limit)
+
+    if downloader.download_file():
+        downloader.decompress_file()
+        # Pass the decompressed filename to the annotation function
+        if not downloader.run_annotation(output_filename.rstrip('.gz')):
+            logging.error("Annotation process failed.")
