123e7b0801b37525bcdfbb60173c30605af81602
julielbr <83019635+julielbr@users.noreply.github.com>
2024-04-27 12:25:30 +0200

    Update transformData.py
    added explenation to the code

--- transformData.py
+++ transformData.py
@@ -1,5 +1,6 @@
 import csv
-"Class to transform the data from the annotated vcf file to an csv file"
+
+"""Class to transform the data from the annotated vcf file to an csv file"""
 def parse_info(info):
     info_dict = {}
     for entry in info.split(';'):
@@ -9,8 +10,10 @@ def parse_info(info):
             info_dict[key] = value
     return info_dict
 
+# Function that takes a VCF file and transform it to a CSV file
 def transform_vcf_to_csv(vcf_filename, csv_filename):
     with open(vcf_filename, 'r') as vcf_file, open(csv_filename, 'w', newline='') as csv_file:
+        # Define the column headers for the CSV file
         fieldnames = ['CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER', 'ALLELEID', 'CLNDISDB', 'CLNDN', 'CLNHGVS', 'CLNSIG', 'CLNVI']
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
@@ -20,7 +23,7 @@ def transform_vcf_to_csv(vcf_filename, csv_filename):
             if not line.startswith('#'):
                 columns = line.strip().split('\t')
                 info_dict = parse_info(columns[7])
-                
+                # Create a dictionary representing the current row of data
                 row = {
                     'CHROM': columns[0],
                     'POS': columns[1],
@@ -39,6 +42,6 @@ def transform_vcf_to_csv(vcf_filename, csv_filename):
                 
                 writer.writerow(row)
 
-
+#!! remember to cange from the test to the clinvar_annotated.vcf file!
 transform_vcf_to_csv('clinvar_annotated.vcf', 'clinvar.csv')
 
