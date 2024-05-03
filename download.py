bc1cf0f845ba26f1bad2affac03de60ff2f4f884
julielbr <83019635+julielbr@users.noreply.github.com>
2024-05-04 00:01:53 +0200

    Update and rename downloadData.py to download.py

---
+++ download.py
@@ -0,0 +1,19 @@
+import requests
+import gzip
+import shutil
+
+def download_and_decompress(url, output_path):
+    # Send a GET request to the URL
+    response = requests.get(url, stream=True)
+    
+    if response.status_code == 200:
+        with open(output_path, 'wb') as f_out, gzip.GzipFile(fileobj=response.raw, mode='rb') as gz:
+            shutil.copyfileobj(gz, f_out)
+        print(f"File downloaded and decompressed to {output_path}")
+    else:
+        print(f"Failed to download the file. Status code: {response.status_code}")
+
+# URL to the ClinVar VCF file
+url = "https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/clinvar.vcf.gz"
+output_path = "clinvar.vcf"
+download_and_decompress(url, output_path)
