dad8f40205eeaef5012c102b09b6ef653bfb0ef0
julielbr <83019635+julielbr@users.noreply.github.com>
2024-05-05 10:58:11 +0200

    Update annotate.py

--- annotate.py
+++ annotate.py
@@ -1,8 +1,8 @@
 import subprocess
 
-# Path to SnpEff executable
+# Path to SnpEff 
 snpeff_path = "/Users/julielervagbreivik/DSG/snpEff.jar"
-# Path to input VCF file
+# Path to input VCF file -change to the clinvar.vcf for the real dataset!
 input_vcf = "test.vcf"
 
 # Path to output annotated VCF file
@@ -11,8 +11,7 @@ output_vcf = "test_annotated.vcf"
 # Command to run SnpEff annotation
 command = f"java -jar {snpeff_path} -v GRCh37.75 {input_vcf} > {output_vcf}"
 
-# Run the command
+# Run
 subprocess.run(command, shell=True)
-
 print("Annotation complete!")
 
